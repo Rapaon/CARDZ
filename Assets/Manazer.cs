2d0557c38eae86665b4f8c716aee5b10eb12f807
TOKAS321 <154322665+TOKAS321@users.noreply.github.com>
2024-01-30 12:21:36 +0100

    balik manazer script

--- Assets/Manazer.cs
+++ Assets/Manazer.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Manazer : MonoBehaviour
+
+    
 {
+    private List<GameObject> balik = new List<GameObject>();
+    public GameObject prefabKarty;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < 11; i++)
+        {
+        balik.Add(Instantiate( prefabKarty,
+                                new Vector3(i,0,0),
+                                Quaternion.identity
+                                )
+            );
+
+        }
     }
 
     // Update is called once per frame
